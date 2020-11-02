563d271951c33a510045151f113a520e26abccdb
Kirsten Szeto <kszeto20@stuy.edu>
2020-11-01 19:26:21 -0500

    added constructor

---
+++ Point.java
@@ -0,0 +1,16 @@
+public class Point {
+
+  private double x;
+  private double y;
+
+  public Point(double X, double Y) {
+    x = X;
+    y = Y;
+  }
+
+  public Point(Point p) {
+    x = p.x;
+    y = p.y;
+  }
+
+}
